4cdb0dcb17c9962c5c5c8c22f7a775a89a3125ba
entronad <entronad@gmail.com>
2025-08-06 16:12:29 +0800

    reconstruction 5

---
+++ lib/evpkdf.ts
@@ -0,0 +1,120 @@
+import {
+  Base,
+  WordArray,
+  Hasher,
+} from './core.js';
+import { MD5Algo } from './md5.js';
+
+interface EvpKDFCfg {
+  keySize?: number;
+  hasher?: typeof MD5Algo;
+  iterations?: number;
+}
+
+/**
+ * This key derivation function is meant to conform with EVP_BytesToKey.
+ * www.openssl.org/docs/crypto/EVP_BytesToKey.html
+ */
+export class EvpKDFAlgo extends Base {
+  cfg: EvpKDFCfg;
+
+  /**
+   * Initializes a newly created key derivation function.
+   *
+   * @param {Object} cfg (Optional) The configuration options to use for the derivation.
+   *
+   * @example
+   *
+   *     const kdf = CryptoJS.algo.EvpKDF.create();
+   *     const kdf = CryptoJS.algo.EvpKDF.create({ keySize: 8 });
+   *     const kdf = CryptoJS.algo.EvpKDF.create({ keySize: 8, iterations: 1000 });
+   */
+  constructor(cfg?: EvpKDFCfg) {
+    super();
+
+    /**
+     * Configuration options.
+     *
+     * @property {number} keySize The key size in words to generate. Default: 4 (128 bits)
+     * @property {Hasher} hasher The hash algorithm to use. Default: MD5
+     * @property {number} iterations The number of iterations to perform. Default: 1
+     */
+    this.cfg = Object.assign(
+      {},
+      {
+        keySize: 128 / 32,
+        hasher: MD5Algo,
+        iterations: 1,
+      },
+      cfg,
+    );
+  }
+
+  /**
+   * Derives a key from a password.
+   *
+   * @param {WordArray|string} password The password.
+   * @param {WordArray|string} salt A salt.
+   *
+   * @return {WordArray} The derived key.
+   *
+   * @example
+   *
+   *     const key = kdf.compute(password, salt);
+   */
+  compute(password: WordArray | string, salt: WordArray | string): WordArray {
+    let block: WordArray | undefined;
+
+    // Shortcut
+    const { cfg } = this;
+
+    // Init hasher
+    const hasher = new cfg.hasher!();
+
+    // Initial values
+    const derivedKey = WordArray.create();
+
+    // Shortcuts
+    const derivedKeyWords = derivedKey.words;
+    const { keySize, iterations } = cfg;
+
+    // Generate key
+    while (derivedKeyWords.length < keySize!) {
+      if (block) {
+        hasher.update(block);
+      }
+      block = hasher.update(password).finalize(salt);
+      hasher.reset();
+
+      // Iterations
+      for (let i = 1; i < iterations!; i += 1) {
+        block = hasher.finalize(block);
+        hasher.reset();
+      }
+
+      derivedKey.concat(block);
+    }
+    derivedKey.sigBytes = keySize! * 4;
+
+    return derivedKey;
+  }
+}
+
+/**
+ * Derives a key from a password.
+ *
+ * @param {WordArray|string} password The password.
+ * @param {WordArray|string} salt A salt.
+ * @param {Object} cfg (Optional) The configuration options to use for this computation.
+ *
+ * @return {WordArray} The derived key.
+ *
+ * @static
+ *
+ * @example
+ *
+ *     var key = CryptoJS.EvpKDF(password, salt);
+ *     var key = CryptoJS.EvpKDF(password, salt, { keySize: 8 });
+ *     var key = CryptoJS.EvpKDF(password, salt, { keySize: 8, iterations: 1000 });
+ */
+export const EvpKDF = (password: WordArray | string, salt: WordArray | string, cfg?: EvpKDFCfg): WordArray => new EvpKDFAlgo(cfg).compute(password, salt);
---
+++ lib/rc4.ts
@@ -0,0 +1,125 @@
+import {
+  StreamCipher,
+  CipherObj,
+} from './cipher-core.js';
+
+function generateKeystreamWord(this: RC4Algo): number {
+  // Shortcuts
+  const S = this._S;
+  let i = this._i;
+  let j = this._j;
+
+  // Generate keystream word
+  let keystreamWord = 0;
+  for (let n = 0; n < 4; n += 1) {
+    i = (i + 1) % 256;
+    j = (j + S[i]) % 256;
+
+    // Swap
+    const t = S[i];
+    S[i] = S[j];
+    S[j] = t;
+
+    keystreamWord |= S[(S[i] + S[j]) % 256] << (24 - n * 8);
+  }
+
+  // Update counters
+  this._i = i;
+  this._j = j;
+
+  return keystreamWord;
+}
+
+/**
+ * RC4 stream cipher algorithm.
+ */
+export class RC4Algo extends StreamCipher {
+  static keySize = 256 / 32;
+  static ivSize = 0;
+
+  protected _S!: number[];
+  protected _i!: number;
+  protected _j!: number;
+
+  _doReset(): void {
+    // Shortcuts
+    const key = this._key;
+    const keyWords = key.words;
+    const keySigBytes = key.sigBytes;
+
+    // Init sbox
+    this._S = [];
+    const S = this._S;
+    for (let i = 0; i < 256; i += 1) {
+      S[i] = i;
+    }
+
+    // Key setup
+    for (let i = 0, j = 0; i < 256; i += 1) {
+      const keyByteIndex = i % keySigBytes;
+      const keyByte = (keyWords[keyByteIndex >>> 2] >>> (24 - (keyByteIndex % 4) * 8)) & 0xff;
+
+      j = (j + S[i] + keyByte) % 256;
+
+      // Swap
+      const t = S[i];
+      S[i] = S[j];
+      S[j] = t;
+    }
+
+    // Counters
+    this._j = 0;
+    this._i = this._j;
+  }
+
+  _doProcessBlock(M: number[], offset: number): void {
+    const _M = M;
+
+    _M[offset] ^= generateKeystreamWord.call(this);
+  }
+}
+
+/**
+ * Shortcut functions to the cipher's object interface.
+ *
+ * @example
+ *
+ *     var ciphertext = CryptoJS.RC4.encrypt(message, key, cfg);
+ *     var plaintext  = CryptoJS.RC4.decrypt(ciphertext, key, cfg);
+ */
+export const RC4: CipherObj = StreamCipher._createHelper(RC4Algo);
+
+/**
+ * Modified RC4 stream cipher algorithm.
+ */
+export class RC4DropAlgo extends RC4Algo {
+  constructor(...args: any[]) {
+    super(...args);
+
+    /**
+     * Configuration options.
+     *
+     * @property {number} drop The number of keystream words to drop. Default 192
+     */
+    Object.assign(this.cfg, { drop: 192 });
+  }
+
+  _doReset(): void {
+    super._doReset.call(this);
+
+    // Drop
+    for (let i = this.cfg.drop; i > 0; i -= 1) {
+      generateKeystreamWord.call(this);
+    }
+  }
+}
+
+/**
+ * Shortcut functions to the cipher's object interface.
+ *
+ * @example
+ *
+ *     var ciphertext = CryptoJS.RC4Drop.encrypt(message, key, cfg);
+ *     var plaintext  = CryptoJS.RC4Drop.decrypt(ciphertext, key, cfg);
+ */
+export const RC4Drop: CipherObj = StreamCipher._createHelper(RC4DropAlgo);
